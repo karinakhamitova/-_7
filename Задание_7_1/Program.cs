149a0b565cdb3581d4e092aa8aeae38639c58ffc
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-04 21:03:34 +0300

    Добавьте файлы проекта.

---
+++ Задание_7_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7_1
+{
+    internal class Program
+    {
+        static double GetTriangleSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return (Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника 1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите стороны треугольника 2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            if (GetTriangleSquare(x1, y1, z1) > GetTriangleSquare(x2, y2, z2))
+            {
+                Console.WriteLine("Площадь треугольника 1 больше");
+            }
+            else if (GetTriangleSquare(x1, y1, z1) < GetTriangleSquare(x2, y2, z2))
+            {
+                Console.WriteLine("Площадь треугольника 2 больше");
+            }
+            else 
+            {
+                Console.WriteLine("Площадь треугольников равны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание_7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7_2
+{
+    internal class Program
+    {
+        static void GetCubeParam(double a, out double S, out double V)
+        {
+            S = 6 * a * a;
+            V = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double S, V;
+            GetCubeParam(a, out S, out V);
+            Console.WriteLine("S = {0:.00}, V = {1:.00}",S,V);
+            Console.ReadKey();  
+        }
+    }
+}
